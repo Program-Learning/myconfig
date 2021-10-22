0b209ccdb9fa93bd6bb13c69e0f8eba0a6d9fe6c
opensourcefuture <1770747317@qq.com>
2021-10-22 19:37:28 +0800

    renamed:    nvim/.config_coc_extensions_package.json -> linux/.config/nvim/.config_coc_extensions_package.json 	renamed:    nvim/UltiSnips/go.snippets -> linux/.config/nvim/UltiSnips/go.snippets 	renamed:    nvim/UltiSnips/html.snippets -> linux/.config/nvim/UltiSnips/html.snippets 	renamed:    nvim/coc-settings.json -> linux/.config/nvim/coc-settings.json 	renamed:    nvim/default_configs/_machine_specific_default.vim -> linux/.config/nvim/default_configs/_machine_specific_default.vim 	renamed:    nvim/init.vim -> linux/.config/nvim/init.vim 	renamed:    nvim/init.vim.bak -> linux/.config/nvim/init.vim.bak 	renamed:    nvim/md-snippets.vim -> linux/.config/nvim/md-snippets.vim 	renamed:    nvim/vimspector_json/go.json -> linux/.config/nvim/vimspector_json/go.json 	renamed:    nvim/vimspector_json/python.json -> linux/.config/nvim/vimspector_json/python.json 	renamed:    nvim/vimspector_json/rust.json -> linux/.config/nvim/vimspector_json/rust.json 	renamed:    nvim/vimspector_json/shell.json -> linux/.config/nvim/vimspector_json/shell.json 	renamed:    nvim2/.gitignore -> linux/.config/nvim2/.gitignore 	renamed:    nvim2/README.md -> linux/.config/nvim2/README.md 	renamed:    nvim2/UltiSnips/go.snippets -> linux/.config/nvim2/UltiSnips/go.snippets 	renamed:    nvim2/UltiSnips/html.snippets -> linux/.config/nvim2/UltiSnips/html.snippets 	renamed:    nvim2/coc-settings.json -> linux/.config/nvim2/coc-settings.json 	renamed:    nvim2/default_configs/_machine_specific_default.vim -> linux/.config/nvim2/default_configs/_machine_specific_default.vim 	renamed:    nvim2/init.vim -> linux/.config/nvim2/init.vim 	renamed:    nvim2/md-snippets.vim -> linux/.config/nvim2/md-snippets.vim 	renamed:    nvim2/preview.jpg -> linux/.config/nvim2/preview.jpg 	renamed:    nvim2/vimspector_json/go.json -> linux/.config/nvim2/vimspector_json/go.json 	renamed:    nvim2/vimspector_json/python.json -> linux/.config/nvim2/vimspector_json/python.json 	renamed:    nvim2/vimspector_json/rust.json -> linux/.config/nvim2/vimspector_json/rust.json 	renamed:    nvim2/vimspector_json/shell.json -> linux/.config/nvim2/vimspector_json/shell.json 	renamed:    ranger/__pycache__/devicons.cpython-39.opt-1.pyc -> linux/.config/ranger/__pycache__/devicons.cpython-39.opt-1.pyc 	renamed:    ranger/commands.py -> linux/.config/ranger/commands.py 	renamed:    ranger/devicons.py -> linux/.config/ranger/devicons.py 	renamed:    ranger/plugins/__init__.py -> linux/.config/ranger/plugins/__init__.py 	renamed:    ranger/plugins/__pycache__/__init__.cpython-39.opt-1.pyc -> linux/.config/ranger/plugins/__pycache__/__init__.cpython-39.opt-1.pyc 	renamed:    ranger/plugins/__pycache__/devicons_linemode.cpython-39.opt-1.pyc -> linux/.config/ranger/plugins/__pycache__/devicons_linemode.cpython-39.opt-1.pyc 	renamed:    ranger/plugins/devicons_linemode.py -> linux/.config/ranger/plugins/devicons_linemode.py 	renamed:    ranger/rc.conf -> linux/.config/ranger/rc.conf 	renamed:    ranger/scope.sh -> linux/.config/ranger/scope.sh 	new file:   termux/.config/neofetch/config.conf 	new file:   termux/.ssh/authorized_keys 	new file:   termux/.ssh/id_ed25519 	new file:   termux/.ssh/id_ed25519.pub 	new file:   termux/.ssh/known_hosts 	new file:   termux/.ssh/known_hosts.old 	new file:   termux/.termux/boot/start-adbd 	new file:   termux/.termux/boot/start-sshd 	new file:   termux/.termux/shell 	new file:   termux/.termux/termux.properties 	new file:   termux/auto_termux_setting.sh 	new file:   termux/termux-installed.txt

---
+++ termux/auto_termux_setting.sh
@@ -0,0 +1,3 @@
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list && sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list && sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list && apt update && apt upgrade 
+
+pkg reinstall pacman man neofetch nodejs nasm android-tools git tsu openssh vim nano ranger clang golang termux-api neovim curl wget tree
