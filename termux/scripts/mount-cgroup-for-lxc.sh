5ee8355228afd045d0cebdc0dc2f10fe1c02f659
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-03-13 01:01:22 +0800

    Fix

--- termux/scripts/mount-cgroup-for-lxc.sh
+++ termux/scripts/mount-cgroup-for-lxc.sh
@@ -2,4 +2,5 @@ mount -t tmpfs -o mode=755 tmpfs /sys/fs/cgroup
 mkdir -p /sys/fs/cgroup/devices
 mount -t cgroup -o devices cgroup /sys/fs/cgroup/devices
 
-lxc-setup-cgroups #可能不需要
+#lxc-setup-cgroups #可能不需要
+/data/data/com.termux/files/usr/bin/lxc-setup-cgroups
